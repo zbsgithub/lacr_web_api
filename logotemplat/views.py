c0508184e2fed2f4483f03d24ffe4ad8178556d7
dk_mcu <dk_mcu@163.com>
2018-11-16 10:18:55 +0800

    add view

--- logotemplat/views.py
+++ logotemplat/views.py
@@ -8,3 +8,12 @@ class LogoTempView(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = LogoTemplateSerializer
     queryset = LogoTemplate.objects.all()
     filter_class = LogoTempFilters
+
+    @staticmethod
+    def get_machine_list(queryset):
+        return queryset.values_list("machine")
+
+    def get_queryset(self):
+        machine_list = int(self.request.query_params.get("machine_list", 0))
+        if machine_list == 1:
+            self.get_machine_list(self.queryset)
