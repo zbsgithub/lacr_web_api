847ecf5d571915487f94782064614b04d2f830e3
dk_mcu <dk_mcu@163.com>
2018-11-30 15:49:07 +0800

    add statistic

--- statistics_info/admin.py
+++ statistics_info/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import CompanyDeviceStatistic, BrandDeviceStatistic, SlaveDeviceStatistic, \
+    CompanyImgStatistic, BrandImgStatistic
 
-# Register your models here.
+
+class CompanyDeviceStatisticAdmin(admin.ModelAdmin):
+    list_display = ("id", "company", "num", "created_at", )
+admin.site.register(CompanyDeviceStatistic, CompanyDeviceStatisticAdmin)
+
+
+class BrandDeviceStatisticAdmin(admin.ModelAdmin):
+    list_display = ("id", "brand", "num", "created_at", )
+admin.site.register(BrandDeviceStatistic, BrandDeviceStatisticAdmin)
+
+
+class SlaveDeviceStatisticAdmin(admin.ModelAdmin):
+    list_display = ("id", "slave", "num", "created_at", )
+admin.site.register(SlaveDeviceStatistic, SlaveDeviceStatisticAdmin)
+
+
+class CompanyImgStatisticAdmin(admin.ModelAdmin):
+    list_display = ("id", "company", "num", "created_at", )
+admin.site.register(CompanyImgStatistic, CompanyImgStatisticAdmin)
+
+
+class BrandImgStatisticAdmin(admin.ModelAdmin):
+    list_display = ("id", "brand", "num", "created_at", )
+admin.site.register(BrandImgStatistic, BrandImgStatisticAdmin)
--- system_info/admin.py
+++ system_info/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ChannelType, ChannelName
+from .models import ChannelType, ChannelName, Slave
 
 
 class ChannelTypeAdmin(admin.ModelAdmin):
@@ -10,3 +10,9 @@ admin.site.register(ChannelType, ChannelTypeAdmin)
 class ChannelNameAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "alias", "chid", "classify", "image")
 admin.site.register(ChannelName, ChannelNameAdmin)
+
+
+class SlaveAdmin(admin.ModelAdmin):
+    list_display = ("id", "mac", "created_at", "created_at", )
+admin.site.register(Slave, SlaveAdmin)
+
