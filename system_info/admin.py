3f2bb96654aaa4ff06867b88873c7d537c306a54
dk_mcu <dk_mcu@163.com>
2018-12-05 17:12:45 +0800

    add admin

--- system_info/admin.py
+++ system_info/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ChannelType, ChannelName, Slave, Company, Brand
+from .models import StdChName, AliasChName, Slave, Company, Brand
 
 
 class CompanyAdmin(admin.ModelAdmin):
@@ -12,14 +12,14 @@ class BrandAdmin(admin.ModelAdmin):
 admin.site.register(Brand, BrandAdmin)
 
 
-class ChannelTypeAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "alias")
-admin.site.register(ChannelType, ChannelTypeAdmin)
+class StdChNameAdmin(admin.ModelAdmin):
+    list_display = ("id", "ch_id", "name", "image", "created_at", "updated_at", )
+admin.site.register(StdChName, StdChNameAdmin)
 
 
-class ChannelNameAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "alias", "chid", "classify", "image")
-admin.site.register(ChannelName, ChannelNameAdmin)
+class AliasChNameAdmin(admin.ModelAdmin):
+    list_display = ("id", "std_ch", "name", "created_at", "updated_at", )
+admin.site.register(AliasChName, AliasChNameAdmin)
 
 
 class SlaveAdmin(admin.ModelAdmin):
