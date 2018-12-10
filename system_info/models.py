a89d40511f7c38fdb5bffcdfc738631a09526af9
dk_mcu <dk_mcu@163.com>
2018-12-10 11:50:30 +0800

    image

--- system_info/models.py
+++ system_info/models.py
@@ -36,7 +36,7 @@ class Brand(models.Model):
 class StdChName(models.Model):
     ch_id = models.CharField(max_length=65, default=None, verbose_name="频道id", unique=True, help_text="频道id")
     name = models.CharField(max_length=200, verbose_name="频道名称", help_text="频道名称")
-    image = models.ImageField(upload_to="system_info/std_channel/", null=True,
+    image = models.ImageField(upload_to="system_info/std_channel/", blank=True, null=True,
                               verbose_name="频道图片", help_text="频道图片")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间", help_text="创建时间")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间", help_text="更新时间")
@@ -45,6 +45,7 @@ class StdChName(models.Model):
         verbose_name = "频道名称信息"
         verbose_name_plural = verbose_name
         db_table = "std_channel"
+        ordering = ("name", )
 
     def __str__(self):
         return self.name
@@ -65,6 +66,7 @@ class AliasChName(models.Model):
         verbose_name = "频道别名"
         verbose_name_plural = verbose_name
         db_table = "alias_channel"
+        ordering = ("name", )
 
     def __str__(self):
         return self.name
