928023cccc938c9371c9d3cf4564225fc19d57f5
BingShuai Zhang <zhangbingshuai@gzads.com>
2018-12-11 11:13:12 +0800

    修改匹配url

--- statistics_info/urls.py
+++ statistics_info/urls.py
@@ -18,9 +18,9 @@ router = DefaultRouter()
 urlpatterns = [
     # path('mac_overview', views.StatisticsInfoView.as_view(),name='mac_overview'),
     # path('brand_trend', views.StatisticsInfoView.as_view(),name='brand_trend'),
-    path('mac_overview', views.MacOverView.as_view()),
-    path('brand_trend', views.BrandTrendView.as_view()),
+    re_path('^mac_overview', views.MacOverView.as_view()),
+    re_path('^brand_trend', views.BrandTrendView.as_view()),
     re_path('^login', views.LoginView.as_view()),
-    path('get_info', views.GetUserInfo.as_view()),
+    re_path('^get_info', views.GetUserInfo.as_view()),
     # re_path('^', include(router.urls))
 ]
